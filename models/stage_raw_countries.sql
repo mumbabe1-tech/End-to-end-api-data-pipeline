677096ea61f16e1e6a80c668ef523f5c3d158332
mumbabe1-tech <mumbabe1@gmail.com>
2026-07-23 21:31:38 +0200

    resolve invalid identifier error by mapping RAW_DATA to src column

--- models/stage_raw_countries.sql
+++ models/stage_raw_countries.sql
@@ -1,33 +1,20 @@
--- Purpose:
---   Convert raw JSON country data into a clean staging table
---   for downstream transformations in the Triplens project.
+-- Purpose: Clean raw country JSON into structured columns
 ------------------------------------------------------------
 
-WITH raw_country_data AS (
-    SELECT 
-        RAW_DATA,
-        INGESTED_AT
-    FROM TRIPLENS_DB.RAW.RAW_COUNTRIES
-)
-
 SELECT
-    -- Country identifiers
-    RAW_DATA:alpha3Code::STRING AS country_code,
-    RAW_DATA:name::STRING AS country_name,
+    -- Country Identifiers
+    src:cca3::STRING AS country_code,
+    src:name::STRING AS country_name,
 
-    -- Geographic information
-    RAW_DATA:capital::STRING AS capital_city,
-    RAW_DATA:region::STRING AS region,
-    RAW_DATA:subregion::STRING AS subregion,
-
-    -- Demographic information
-    RAW_DATA:population::INTEGER AS population,
-    RAW_DATA:area::FLOAT AS total_area_square_kilometers,
-
-    -- Membership information
-    RAW_DATA:independent::BOOLEAN AS is_un_member,
+    -- Geographic & Demographic Info
+    src:capital[0]::STRING AS capital_city,
+    src:region::STRING AS region,
+    src:subregion::STRING AS subregion,
+    src:population::INTEGER AS population,
+    src:area::FLOAT AS total_area_square_kilometers,
 
     -- Metadata
-    INGESTED_AT
-FROM raw_country_data
-WHERE RAW_DATA:alpha3Code IS NOT NULL
+    loaded_at AS ingested_at
+
+FROM TRIPLENS_DB.RAW.RAW_COUNTRIES
+WHERE src:cca3 IS NOT NULL
