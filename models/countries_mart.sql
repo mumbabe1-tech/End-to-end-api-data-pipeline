b6e6cb7745a28633729dacd72ec59af302142173
mumbabe1-tech <mumbabe1@gmail.com>
2026-07-31 10:28:42 +0200

    updated

--- models/countries_mart.sql
+++ models/countries_mart.sql
@@ -6,61 +6,46 @@
     )
 }}
 
-with countries as (
-  SELECT
-    -- Top level keys
-    country.value:name:common::STRING AS country_name,
-    country.value:name:official::STRING AS official_name,
-    country.value:capital[0]::STRING AS capital_city,
-    country.value:callingCodes[0]::STRING AS calling_codes,
-    country.value:unMember::BOOLEAN AS government_type,
-    country.value:population::NUMBER AS population,
-    country.value:region::STRING AS region,
-    country.value:subregion::STRING AS subregion,
+SELECT
+    stg.country AS country_name,
+    stg.official_name AS official_name,
+    stg.capital_city AS capital_city,
+    stg.country_code AS country_code,
+    NULL AS government_type,
+    stg.population AS population,
+    stg.region AS region,
+    stg.subregion AS subregion,
     
-    -- Array keys & attributes
-    country.value:continents[0]::STRING AS continent,
-    country.value:independent::STRING AS independent,
-    country.value:area::NUMBER AS kilometers,
-    country.value:area::NUMBER AS miles,
-    country.value:landlocked::STRING AS landlocked,
-    country.value:unMember::BOOLEAN AS un_member,
-    country.value:independent::BOOLEAN AS eu_member,
+    -- Attributes
+    NULL AS continent,
+    stg.independent AS independent,
+    stg.area_kilometers AS kilometers,
+    stg.area_miles AS miles,
+    stg.landlocked AS landlocked,
+    FALSE AS un_member,
+    FALSE AS eu_member,
     FALSE AS arab_league_member,
     FALSE AS african_union_member,
 
-    country.value:startOfWeek::STRING AS start_of_week,
-    country.value:timezones[0]::STRING AS timezone,
+    stg.timezone AS timezone,
+    stg.start_of_week AS start_of_week,
 
-    -- Visuals & Identifiers (Flags, Coat of Arms, Postal Codes)
-    country.value:flags:png::STRING AS flag_png,
-    country.value:flags:svg::STRING AS flag_svg,
-    country.value:flags:alt::STRING AS flag_alt,
-    country.value:coatOfArms:png::STRING AS coat_of_arms_png,
-    country.value:coatOfArms:svg::STRING AS coat_of_arms_svg,
-    country.value:postalCode:format::STRING AS postal_code_format,
-    country.value:postalCode:regex::STRING AS postal_code_regex,
+    -- Visuals & Identifiers
+    stg.flag_png AS flag_png,
+    stg.flag_svg AS flag_svg,
+    stg.flag_alt AS flag_alt,
+    NULL AS coat_of_arms_png,
+    NULL AS coat_of_arms_svg,
+    NULL AS postal_code_format,
+    NULL AS postal_code_regex,
     
-    -- Dynamic Currency keys
-    currency.key::STRING AS currency_code,
-    currency.value:name::STRING AS currency_name,
-    currency.value:symbol::STRING AS currency_symbol,
+    -- Dynamic placeholders
+    NULL AS currency_code,
+    NULL AS currency_name,
+    NULL AS currency_symbol,
+    NULL AS language_code,
+    NULL AS language_name,
+    NULL AS top_level_domain,
+    NULL AS border_country_code
 
-    -- Dynamic Language keys
-    language.key::STRING AS language_code,
-    language.value::STRING AS language_name,
-
-    -- Additional keys (Top-level domain & Borders)
-    tld.value::STRING AS top_level_domain,
-    border.value::STRING AS border_country_code
-
-  FROM {{ ref('stage_raw_countries') }} AS stg
-    CROSS JOIN LATERAL FLATTEN(input => PARSE_JSON(stg.payload)) country
-    LEFT JOIN LATERAL FLATTEN(input => country.value:currencies) currency
-    LEFT JOIN LATERAL FLATTEN(input => country.value:languages) language
-    -- Left joins prevent dropping rows if a country is missing tld, borders, currencies, or languages
-    LEFT JOIN LATERAL FLATTEN(input => country.value:tld) tld
-    LEFT JOIN LATERAL FLATTEN(input => country.value:borders) border
-)
-
-select * from countries
+FROM {{ ref('stage_raw_countries') }} AS stg
